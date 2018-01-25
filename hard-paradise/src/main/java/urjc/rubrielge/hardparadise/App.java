64c7064384e7af62f59171414be7d0b3d0bf1a2a
SexyBuggy <35573109+SexyBuggy@users.noreply.github.com>
2018-01-25 12:07:25 +0100

    Add files via upload
    Subiendo el proyecto Spring en vacío

---
+++ hard-paradise/src/main/java/urjc/rubrielge/hardparadise/App.java
@@ -0,0 +1,13 @@
+package urjc.rubrielge.hardparadise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
